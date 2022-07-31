25b11b3d1b6b536e5e4543a163f02c836de9bc4d
Elijah Cuff <62396650+ElijahCuff@users.noreply.github.com>
2022-07-31 15:42:02 +1000

    Add files via upload
    Src

---
+++ Website/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2yC4KovjVVHTkkFXtSt0a0vt-q_8sIJDRZQ&usqp=CAU" type="image/x-icon">
+  <meta name="description" content="Scanner aims to collect live sources of the Tasmania Government Radio Network and make them available in one easy to use website, available on Android, Windows, MAC and iPhone device's.">
+ 
+  <title>Tas GRN Scanner Project</title>
+  <link rel="stylesheet" href="media.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="player">
+
+    <div class="player__header">
+
+      <div class="player__img player__img--absolute slider">
+
+        <button class="player__button player__button--absolute--nw playlist">
+
+          <img src="http://physical-authority.surge.sh/imgs/icon/playlist.svg" alt="playlist-icon">
+
+        </button>
+
+        <button class="player__button player__button--absolute--center play">
+
+          <img src="http://physical-authority.surge.sh/imgs/icon/play.svg" alt="play-icon">
+          <img src="http://physical-authority.surge.sh/imgs/icon/pause.svg" alt="pause-icon">
+
+        </button>
+
+        <div class="slider__content">
+          <img class="img slider__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6ww-uyNQUiyq5EoZidnnYi5HOX82k7PlK_Q&usqp=CAU" alt="cover">
+<img class="img slider__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2yC4KovjVVHTkkFXtSt0a0vt-q_8sIJDRZQ&usqp=CAU" alt="cover">
+          <img class="img slider__img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2yC4KovjVVHTkkFXtSt0a0vt-q_8sIJDRZQ&usqp=CAU" alt="cover">
+          <img class="img slider__img" src="https://www.kindpng.com/picc/m/501-5011096_thumb-image-airplane-logo-png-transparent-png.png" alt="cover">
+          <img class="img slider__img" src="https://www.kindpng.com/picc/m/501-5011096_thumb-image-airplane-logo-png-transparent-png.png" alt="cover">
+
+        </div>
+
+      </div>
+
+      <div class="player__controls">
+
+        <button class="player__button back">
+
+          <img class="img" src="http://physical-authority.surge.sh/imgs/icon/back.svg" alt="back-icon">
+
+        </button>
+        
+         
+        <p class="player__context slider__context">
+
+          <strong class="slider__name"></strong>
+          <span class="player__title slider__title"></span>
+
+        </p>
+
+        <button class="player__button next">
+
+          <img class="img" src="http://physical-authority.surge.sh/imgs/icon/next.svg" alt="next-icon">
+
+        </button>
+
+        <div class="progres" style="display: none">
+
+           <div class="progres__filled"></div> 
+
+        </div>
+      </div>
+
+    </div>
+
+    <ul class="player__playlist list">
+
+      
+
+      <li class="player__song">
+
+        <img class="player__img img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6ww-uyNQUiyq5EoZidnnYi5HOX82k7PlK_Q&usqp=CAU" alt="cover">
+
+        <p class="player__context">
+
+          <span class="flex">
+
+            <span class="player__title">Police</span>
+
+          </span>
+          
+          <b class="player__song-name">South</b>
+        </p>
+
+        <audio class="audio" src="https://broadcastify.cdnstream1.com/35239"></audio>
+
+      </li>
+
+      <li class="player__song">
+
+        <img class="player__img img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2yC4KovjVVHTkkFXtSt0a0vt-q_8sIJDRZQ&usqp=CAU" alt="cover">
+
+        <p class="player__context">
+
+          <span class="flex">
+
+            <span class="player__title">Fire</span>
+
+          </span>
+
+          <b class="player__song-name">North</b>
+        </p>
+
+        <audio class="audio" src="https://broadcastify.cdnstream1.com/25485"></audio>
+
+      </li>
+      <li class="player__song">
+
+        <img class="player__img img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2yC4KovjVVHTkkFXtSt0a0vt-q_8sIJDRZQ&usqp=CAU" alt="cover">
+
+        <p class="player__context">
+
+          <span class="flex">
+
+            <span class="player__title">Fire</span>
+
+          </span>
+          
+          <b class="player__song-name">South</b>
+        </p>
+
+        <audio class="audio" src="https://broadcastify.cdnstream1.com/38396"></audio>
+
+      </li>
+      <li class="player__song">
+
+        <img class="player__img img" src="https://www.kindpng.com/picc/m/501-5011096_thumb-image-airplane-logo-png-transparent-png.png" alt="cover">
+
+        <p class="player__context ">
+
+          
+          <span class="flex">
+
+            <span class="player__title">Airport</span>
+         
+
+          </span>
+<b class="player__song-name">North</b>
+        </p>
+
+        <audio class="audio" src="https://s1-fmt2.liveatc.net/ymhb2.mp3?nocache=2022073007371612963"></audio>
+
+      </li>
+
+      <li class="player__song">
+
+        <img class="player__img img" src="https://www.kindpng.com/picc/m/501-5011096_thumb-image-airplane-logo-png-transparent-png.png" alt="cover">
+
+        <p class="player__context">
+
+          
+          <span class="flex">
+
+            <span class="player__title">Airport</span>
+          <!-- <span class="player__song-time"></span> -->
+
+          </span>
+<b class="player__song-name">South</b>
+        </p>
+
+        <audio class="audio" src="https://s1-fmt2.liveatc.net/ymhb?nocache=2022073007371612963"></audio>
+
+      </li>
+
+      
+
+    </ul>
+
+<div>
+<div id="container" align="center" style="width: 80px; border: 0px solid black; float:left;">
+<p id="title" align="center" style="
+    float: top;
+    margin:0px 2px 0px 2px;
+    colour:black">North Police</p>
+<div style="margin:1px auto; border: 0px;  overflow: hidden;max-width: 58px;max-height: 58px;border-radius: 120px;">
+<iframe scrolling="no" src="https://www.caster.fm/widgets/em_player.php?jsinit=true&uid=541413&t=black&c=&pop=true" style="width:400px; height: 200px; border: -1px none;margin-left: -278.2px; margin-top: -108.5px;">
+</iframe>
+</div>
+</div>
+
+<div id="container" align="center" style="width: 80px; border: 0px solid black; float:right;">
+<p id="title" align="center" style="
+    float: top;
+    margin:0px 2px 0px 2px;
+    colour:black">South Police 2</p>
+<div style="margin:1px auto; border: 0px;  overflow: hidden;max-width: 58px;max-height: 58px;border-radius: 120px;">
+<iframe scrolling="no" src="https://www.caster.fm/widgets/em_player.php?jsinit=true&uid=545397&t=black&c=&pop=true" style="width:400px; height: 200px; border: -1px none;margin-left: -278.2px; margin-top: -108.5px;">
+</iframe>
+</div>
+</div>
+</div>
+
+
+</div>
+
+
+<!-- partial -->
+  <script  src="media.js"></script>
+
+</body> 
+</html>
